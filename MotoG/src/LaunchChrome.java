19eaff1f639edf40371c24331b22eae9a7b7e96a
tajkumarkempaiah <tajkumarkempaiah@192.168.2.3>
2018-06-17 07:50:02 +0530

    Added comment from Eclipse

--- MotoG/src/LaunchChrome.java
+++ MotoG/src/LaunchChrome.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome 
 {
-	
+	//Comment from Eclipse
 	public static void main(String[] args) throws InterruptedException 
 	{
 		WebDriver driver = new ChromeDriver();
